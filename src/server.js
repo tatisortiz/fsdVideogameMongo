fad1d53e84a20680d31a5dfabd0fd3964a900fd9
tatianaD <angietatiana167@gmail.com>
2024-07-10 12:30:33 +0200

    feat:server running

--- src/server.js
+++ src/server.js
@@ -1,14 +1,31 @@
 //const express= require('express')
-
-
 import  express from 'express';
 import 'dotenv/config';
+import { dbConnection } from './database/db.js';
 
 const app = express()
 
 const PORT =process.env.PORT || 4000
 
 
-app.listen(PORT, () => {
-    console.log(`server runnig ${PORT}`);
+app.get('/healthy',(req, res)=> {
+    res.json(
+        {
+            success: true,
+            message:"Server is healthy"
+        }
+    )
 })
+
+
+dbConnection().then(()=> {
+    console.log('Databse connected');
+    app.listen(PORT, () => {
+        console.log(`server runnig ${PORT}`);
+    })
+})
+
+.catch (error => {
+    console.log('Error connection database:'+ error);
+})
+
